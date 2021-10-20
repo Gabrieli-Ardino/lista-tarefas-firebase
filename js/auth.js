f198ca65333d3bb4df73253ebe66a4eade209e21
Gabrieli Ardino <gabrieliardino@gmail.com>
2021-10-20 11:29:35 -0300

    Primeiro commit

---
+++ js/auth.js
@@ -0,0 +1,98 @@
+function login() {
+    if (firebase.auth().currentUser) {
+      firebase.auth().signOut()
+    }
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        swal
+          .fire({
+            icon: "success",
+            title: "Certinho. Seja Bem Vindo!",
+          })
+          .then(() => {
+            setTimeout(() => {
+              window.location.replace("página.html")
+            }, 1000)
+          })
+      })
+      .catch((error) => {
+        const errorCode = error.code
+        switch (errorCode) {
+          case "auth/wrong-password":
+            swal.fire({
+              icon: "error",
+              title: "Senha incorreta",
+            })
+            break
+          case "auth/invalid-email":
+            swal.fire({
+              icon: "error",
+              title: "E-mail inválido",
+            })
+            break
+          case "auth/user-not-found":
+            swal
+              .fire({
+                icon: "warning",
+                title: "Esse usuário não existe.",
+                text: "Deseja criar esse usuário?",
+                showCancelButton: true,
+                cancelButtonText: "Não",
+                cancelButtonColor: "#B22222",
+                confirmButtonText: "Sim",
+                confirmButtonColor: "#32CD32",
+              })
+              .then((result) => {
+                if (result.value) {
+                  signUp(email, password)
+                }
+              })
+            break
+          default:
+            swal.fire({
+              icon: "error",
+              title: error.message,
+            })
+        }
+      })
+  }
+  
+  function signUp(email, password) {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        swal
+          .fire({ icon: "success", title: "Usuário foi criado com sucesso" })
+          .then(() => {
+            setTimeout(() => {
+              window.location.replace("página.html")
+            }, 1000)
+          })
+      })
+      .catch((error) => {
+        const errorCode = error.code
+        switch (errorCode) {
+          case "auth/weak-password":
+            swal.fire({
+              icon: "error",
+              title: "Senha muito fraca",
+            })
+            break
+          default:
+            swal.fire({
+              icon: "error",
+              title: error.message,
+            })
+        }
+      })
+  }
+  
+  function logout() {
+    firebase.auth().signOut()
+  }
+    
---
+++ js/init.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC_pwp1pqkalt2_D6zm5XGsJZIcaU-E4IM",
+    authDomain: "todo-list-1df63.firebaseapp.com",
+    projectId: "todo-list-1df63",
+    storageBucket: "todo-list-1df63.appspot.com",
+    messagingSenderId: "896789253227",
+    appId: "1:896789253227:web:0241e290673fc4ca6c752e"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
